add099040b5e72f11b656085cc2f20e7413da516
Cristian Molina <cmutep@gmail.com>
2022-06-18 16:45:11 -0600

    arrays

---
+++ jsCheatSheet.js
@@ -0,0 +1,16 @@
+//--Array--
+// length
+let arr = [1, 2, 3]
+console.log(arr, arr.length)
+
+//multidimentions
+let a = [
+	[1, 2, 3],
+	[1, 2, 3],
+	[1, 2, 3]
+]
+console.log(a, arr.length)
+
+//set a fixed size
+let nItems = new Array(1000)
+console.log(nItems, nItems.length)
