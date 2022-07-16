74ae2c1c4db23e43478d1ec25e066f7d80353d92
Cristian Molina <cmutep@gmail.com>
2022-07-15 20:15:44 -0600

    fixes classphotos

--- examples.js
+++ examples.js
@@ -3,40 +3,31 @@ function classPhotos(redShirtHeights, blueShirtHeights) {
 	//sort your kids by smallest height (tiny guys first)
 	let redKids = [...redShirtHeights].sort((a, b) => b - a)
 	let blueKids = [...blueShirtHeights].sort((a, b) => b - a)
+	let isEveryRedKidTaller = false
+	let isEveryBlueKidTaller = false
 
-	console.log(redKids);
-
-	// check if there's a kid with the same shirt and same height as another. If there is, return false.
-	let isThereTwoRedKidsWithSameHeight = redKids.some((kidHeight, i) => kidHeight === redKids[i + 1])
-	console.log('two red kids with same height?', isThereTwoRedKidsWithSameHeight);
-	if (isThereTwoRedKidsWithSameHeight) return false
-
-	let isThereTwoBlueKidsWithSameHeight = blueKids.some((kidHeight, i) => kidHeight === blueKids[i + 1])
-	console.log('two blue kids with same height?', isThereTwoBlueKidsWithSameHeight);
-	if (isThereTwoBlueKidsWithSameHeight) return false
+	console.log('red', redKids);
+	console.log('blue', blueKids);
 
 	//find the tallest kid and determine whether he's wearing red or blue.
-	const tallestRed = Math.max(...redKids)
-	const tallestBlue = Math.max(...blueKids)
+	const tallestRed = redKids[0]
+	const tallestBlue = blueKids[0]
 	const shirtColorInFrontRow = tallestRed > tallestBlue ? 'red' : 'blue'
+	console.log(shirtColorInFrontRow);
 
-	for (let i = 0; i < redShirtHeights.length; i++) {
-		let redKidHeight = redKids[i]
-		let blueKidHeight = blueKids[i]
+	if (shirtColorInFrontRow === 'red')
+		isEveryRedKidTaller = redKids.every((redHeight, i) => redHeight > blueKids[i])
 
-		if (shirtColorInFrontRow === 'red') {
-			if (redKidHeight >= blueKidHeight) return false //make sure that all the red kids are smaller than blue kids
-			if (blueKidHeight >= redKidHeight) return false
-		}
+	else
+		isEveryBlueKidTaller = blueKids.every((blueHeight, i) => blueHeight > redKids[i])
 
-	}
 
-	return true
+	return isEveryBlueKidTaller || isEveryRedKidTaller
 
 }
 
-const a = [5, 7, 1, 3, 4]
-const b = [6, 9, 2, 4, 5]
+const blue = [6, 9, 2, 4, 5]
+const red = [5, 8, 1, 3, 4]
 
-const ans = classPhotos(a, b)
+const ans = classPhotos(red, blue)
 console.log('answer:', ans);
