b4b5d3ea2e1b76840e8f58c94fbd10a73959b38e
Cristian Molina <cmutep@gmail.com>
2022-07-15 18:21:15 -0600

    classPhotos

--- examples.js
+++ examples.js
@@ -1,40 +1,42 @@
-'use strict'
+function classPhotos(redShirtHeights, blueShirtHeights) {
+	// Write your code here.
+	//sort your kids by smallest height (tiny guys first)
+	let redKids = [...redShirtHeights].sort((a, b) => b - a)
+	let blueKids = [...blueShirtHeights].sort((a, b) => b - a)
 
-// const singleNumber = function(nums) {
-// 	let map = new Map();
-// 	for (let n of nums) {
-// 		// map.set(n, map.get(n) ? map.get(n) + 1 : 1);
-// 		// map.set(n, (map.get(n) ?? 0) + 1)
-// 	}
-// 	console.log(map);
-//
-// 	for (let [key, val] of map) {
-// 		if (val === 1) {
-// 			return key;
-// 		}
-// 	}
-// };
-//
-// const a = [1, 1, 2, 2, 3, 3, 4, 4, 5]
-// console.log(singleNumber(a));
+	console.log(redKids);
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var singleNumber = function(nums) {
-	let map = new Map()
-	for (let n of nums) {
-		map.set(n, map.get(n) + 1 || 1)
-	}
-	console.log(map);
-	for (let [key, val] of map) {
-		if (val === 1) {
-			return key
+	// check if there's a kid with the same shirt and same height as another. If there is, return false.
+	let isThereTwoRedKidsWithSameHeight = redKids.some((kidHeight, i) => kidHeight === redKids[i + 1])
+	console.log('two red kids with same height?', isThereTwoRedKidsWithSameHeight);
+	if (isThereTwoRedKidsWithSameHeight) return false
+
+	let isThereTwoBlueKidsWithSameHeight = blueKids.some((kidHeight, i) => kidHeight === blueKids[i + 1])
+	console.log('two blue kids with same height?', isThereTwoBlueKidsWithSameHeight);
+	if (isThereTwoBlueKidsWithSameHeight) return false
+
+	//find the tallest kid and determine whether he's wearing red or blue.
+	const tallestRed = Math.max(...redKids)
+	const tallestBlue = Math.max(...blueKids)
+	const shirtColorInFrontRow = tallestRed > tallestBlue ? 'red' : 'blue'
+
+	for (let i = 0; i < redShirtHeights.length; i++) {
+		let redKidHeight = redKids[i]
+		let blueKidHeight = blueKids[i]
+
+		if (shirtColorInFrontRow === 'red') {
+			if (redKidHeight >= blueKidHeight) return false //make sure that all the red kids are smaller than blue kids
+			if (blueKidHeight >= redKidHeight) return false
 		}
+
 	}
 
-};
+	return true
 
-let nums = [2, 2, 1, 3, 3, 4, 4, 5, 5, 6, 6]
-console.log('answer:', singleNumber(nums));
+}
+
+const a = [5, 7, 1, 3, 4]
+const b = [6, 9, 2, 4, 5]
+
+const ans = classPhotos(a, b)
+console.log('answer:', ans);
