d40de3f3344fedc6eff071955e230c39e8d3c194
Cristian Molina <cmutep@gmail.com>
2022-07-25 19:17:21 -0600

    added sets

--- examples.js
+++ examples.js
@@ -1,14 +1,9 @@
-//return all duplicate values in a list
-const a = [1, 1, 2, 2, 3, 3, 4, 5, 6, 7, 7, 7, 8, 9, 9, 9, 9, 9, 9]
-
-function returnDuplicates(arr) {
-	let duplicateNumbers = []
-	let map = new Map()
-	a.forEach(n => map.set(n, map.get(n) + 1 || 1))
-	for (let [key, value] of map) {
-		if (value >= 2) duplicateNumbers.push(key)
-	}
-	return duplicateNumbers
+function containsDuplicates(array) {
+	mySet = new Set(array)
+	console.log(mySet);
+	const hasDuplicate = mySet.length === array.length ? false : true
+	return hasDuplicate
 }
 
-console.log(returnDuplicates(a));
+a = [1, 2, 1, 2, 1, 2, 1, 5, 4, 5, 4, 5, 4, 5]
+console.log(containsDuplicates(a));
