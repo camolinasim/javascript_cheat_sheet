7ebfe8999128a3e66e8b0b43418c01180f3a9ebf
Cristian Molina <cmutep@gmail.com>
2022-07-27 03:53:36 -0600

    variables

---
+++ python_lectures.py
@@ -0,0 +1,46 @@
+#Lecture 2: Variables.
+#Homework video: https://www.youtube.com/watch?v=cQT33yu9pY8
+
+"""
+Variables are simply a way to store data. They are most often used to store primitive data
+
+Formula to create a variable:
+variable_name = some_value
+
+The primitive data types in Python are the following:
+1) numbers (1, 2.5, -4.99, -1)
+2) booleans (True or False values)
+3) strings (sentences wrapped by quotation marks - 'I am a string')
+
+The most important part of variables is NAMING them. Variables MUST have descriptive names that explain what data they represent/store. For example:
+
+DO NOT DO:
+x = 3.141592
+
+instead do:
+pi = 3.141592
+
+Also acceptable:
+value_of_pi = 3.141592
+
+Naming a variable 'x' tells the reader NOTHING about what the variable contains/stores. If we name it 'pi', or 'value_of_pi', it is immediately obvious to the programmer what this variable holds. Remember, code readability is KING in programming, and it separates bad programmers from good ones.
+
+Another very, VERY important concept of variables is called Scoping, and it defines where a variable is accessible/visible. We will look at scoping after we go over functions. Optionally, you can watch this video to get introduced to variable scoping: https://www.youtube.com/watch?v=wueXfiVkw1I
+"""
+              ############# EXERCISE ################
+"""
+Instructions:
+read the problem prompt and write a program to solve it
+
+A new anime series has started airing and they currently have 3 episodes in total. Episode 1, 2 and 3 are 24, 25 and 26 minutes long respectively. Write a program that calculates the total watch time in minutes from the first 3 episodes. Print the result into the console.
+
+Remember to use descriptive variable names.
+"""
+
+"""
+FOR NEXT LECTURE
+Watch the listed videos on the following topics
+
+1) Learn to do string concatenation: https://www.youtube.com/watch?v=OkGFdPX2Bxk
+2) Learn to accept user input: https://www.youtube.com/watch?v=1gEZi0uJ3sw (IGNORE THE EXERCISE FROM MINUTE 5:00)
+"""
