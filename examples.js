27f4c65f441295ce602c7f3cec2da6675b84b32c
Cristian Molina <cmutep@gmail.com>
2022-07-27 03:58:37 -0600

    palindrome

--- examples.js
+++ examples.js
@@ -1,9 +1,31 @@
-function containsDuplicates(array) {
-	mySet = new Set(array)
-	console.log(mySet);
-	const hasDuplicate = mySet.size === array.length ? false : true
-	return hasDuplicate
+function isPalidrome(string) {
+	let reversed = string.split('')
+		.reverse()
+		.join('')
+		.toLocaleLowerCase()
+	let original = string.split('')
+		.join('')
+		.toLowerCase()
+
+	reversed = removeWhiteSpace(reversed.split(''))
+	original = removeWhiteSpace(original.split(''))
+
+
+
+	console.log(reversed);
+	console.log(original);
+
+	return reversed === original
+		.toLowerCase()
+
 }
 
-a = [1, 2, 3]
-console.log(containsDuplicates(a));
+a = "A man, a plan, a canal: Panama"
+console.log(isPalidrome(a));
+
+function removeWhiteSpace(s) {
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] === ' ') s.splice(i, 1)
+	}
+	return s.join('')
+}
